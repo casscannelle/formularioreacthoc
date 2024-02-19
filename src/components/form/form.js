657741f31e12fce6d41273d4ff2d08ace42c675f
casscannelle <cassia.maffei@gmail.com>
2024-02-19 16:44:41 -0300

    duplicar form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -116,60 +116,39 @@ function Form() {
           </button>
         </form>
       </div>
-      {/* Respostas por nome */}
-      <div className={`container-answer ${showAnswers ? '' : 'hide-container-answer'}`}>
-        {showAnswers && (
-          <div>
-            <>
-            <h2>Respostas</h2>
-              <button className="btn-filter" onClick={toggleSortOrder}>
-                {sortOrder === 'newest' ? 'Mais recentes' : 'Mais antigas'}
-              </button>
-              </>
-            
-            
-            <ol>
-              {getSortedResponses().map((response, index) => (
-                <li key={index} >
-                  {response.nome} 
-                  <button className="btn-vermais" onClick={() => showResponseModal(response)}>Ver mais</button>
-                  <button className="btn-vermais" onClick={() => deleteAnswer(index)}>Excluir</button>
-                </li>
-              ))}
-            </ol>
-            <button className='btn' onClick={hideAnswers}>
-              Fechar
-            </button>
-          </div>
-        )}
+      <div className='container-form'>
+        <form>
+        <h1 className='form_h1'>Preencha seus dados</h1>
+        <label>Nome:</label>
+        <input className='form_input' type="text" name="nome" value={inputValue.nome} onChange={handleChange} required />
+        {showAnswers || !errors.nome ? null : <p className="error-message">{errors.nome}</p>}
+        <label>Idade:</label>
+        <input className='form_input' type="number" name="idade" value={inputValue.idade} onChange={handleChange} min="0" required />
+        {showAnswers || !errors.idade ? null : <p className="error-message">{errors.idade}</p>}
+        <label>E-mail:</label>
+        <input className='form_input' type="email" name="email" value={inputValue.email} onChange={handleChange} required />
+        {showAnswers || !errors.email ? null : <p className="error-message">{errors.email}</p>}
+        <label>Gênero</label>
+        <div>
+          <input type='radio' name="gender" value="Feminino" id="feminino" checked={inputValue.gender === "Feminino"} onChange={handleChange} />
+          <label htmlFor="feminino">Feminino</label>
+        </div>
+        <div>
+          <input type='radio' name="gender" value="Masculino" id="masculino" checked={inputValue.gender === "Masculino"} onChange={handleChange} />
+          <label htmlFor="masculino">Masculino</label>
+        </div>
+        <div>
+          <input type='radio' name="gender" value="Não informado" id="naoinformado" checked={inputValue.gender === "Não informado"} onChange={handleChange} />
+          <label htmlFor="naoinformado">Prefiro não informar</label>
+        </div>
+        {showAnswers || !errors.gender ? null : <p className="error-message">{errors.gender}</p>}
+        <button className='btn' type="submit" onClick={handleSubmit}>
+            Enviar
+          </button>
+        </form>
+      </div>
       </div>
 
-
-      {/* Modal com respostas completas */}
-      {selectedResponse && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={hideModal}>
-              &times;
-            </span>
-            <h2>Detalhes da Resposta</h2>
-            <p>
-              <strong>Nome:</strong> {selectedResponse.nome}
-            </p>
-            <p>
-              <strong>Idade:</strong> {selectedResponse.idade}
-            </p>
-            <p>
-              <strong>E-mail:</strong> {selectedResponse.email}
-            </p>
-            <p>
-              <strong>Gênero:</strong> {selectedResponse.gender}
-            </p>
-            <button className="btn" onClick={hideModal}>Fechar</button>
-          </div>
-        </div>
-      )}
-    </div>
   );
 }
 
