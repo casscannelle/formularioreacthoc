1374b1ee9c16c6ed31313fe59333735fa73aa06d
casscannelle <cassia.maffei@gmail.com>
2024-02-20 10:16:34 -0300

    estrutura componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Inscricao from './components/formInscricao/formInscricao';
+import Comentario from './components/formComentario/formComentario';
+import Form from './components/hoc/hoc';
 
 function App() {
   
   return (
     <div>
     
+      <Inscricao />
+      <Comentario />
+      <Form />
       
     </div>
   );
